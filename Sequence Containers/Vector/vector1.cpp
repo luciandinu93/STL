0bdcf802167b50140845caa65d06bb07768c600e
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-03-31 15:19:13 +0300

    vector

--- Sequence Containers/Vector/vector1.cpp
+++ Sequence Containers/Vector/vector1.cpp
@@ -1,21 +1,45 @@
 #include <vector>
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
 
 int main()
 {
-	std::vector<int> coll;  // vector container for integer elements
+	std::vector<std::string> sentence;
 	
-	// append elements with value 1 to 6
-	for(int i = 1; i <= 6; ++i)
-	{
-		coll.push_back(i);
-	}
+	sentence.reserve(5);
 	
-	// print all elements followed by space 
-	for(int i = 0; i < coll.size(); ++i)
-	{
-		std::cout << coll[i] << ' ';
-	}
+	sentence.push_back("Hello,");
+	sentence.insert(sentence.end(), {"how", "are", "you", "?"});
 	
+	std::copy(sentence.cbegin(), sentence.cend(), 
+				std::ostream_iterator<std::string>(std::cout, " "));
+	std::cout << std::endl;			
+	
+	std::cout << " max_size(): " << sentence.max_size() << std::endl;
+	std::cout << " size(): " << sentence.size() << std::endl;
+	std::cout << " capacity(): " << sentence.capacity() << std::endl;
+
+	std::swap(sentence[1], sentence[3]);
+	
+	sentence.insert(find(sentence.begin(), sentence.end(), "?"), "always");
+	
+	sentence.back() = "!";
+	
+	std::copy(sentence.cbegin(), sentence.cend(),
+				std::ostream_iterator<std::string>(std::cout, " "));
 	std::cout << std::endl;
+	
+	std::cout << " max_size(): " << sentence.max_size() << std::endl;
+	std::cout << " size(): " << sentence.size() << std::endl;
+	std::cout << " capacity(): " << sentence.capacity() << std::endl;
+	
+	sentence.pop_back();
+	sentence.pop_back();
+	sentence.shrink_to_fit();
+	
+	std::cout << " max_size(): " << sentence.max_size() << std::endl;
+	std::cout << " size(): " << sentence.size() << std::endl;
+	std::cout << " capacity(): " << sentence.capacity() << std::endl;
 }
