2e3a4caf440eae7e60e55575a391ac25bcd7a14d
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-03-31 15:12:08 +0300

    array

--- Sequence Containers/Arrays/array1.cpp
+++ Sequence Containers/Arrays/array1.cpp
@@ -1,14 +1,35 @@
 #include <array>
-#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
 #include <string>
+#include <iostream>
+
+template<typename I>
+void print(const I& coll, const std::string optstr="")
+{
+	std::cout << optstr;
+	for(const auto& elem : coll)
+		std::cout << elem << " ";
+	std::cout << std::endl;
+}
 
 int main()
 {
-	// array container of 5 string elements
-	std::array<std::string, 5> coll = {"hello", "world"};
+	// create array with 10 ints
+	std::array<int, 10> a = {10, 22, 33, 44};
 	
-	for(int i = 0; i < 100; i++)
-	{
-		std::cout << i << ": " << coll[i] << std::endl;
-	}
+	print(a, "initialized: ");
+	
+	// modify last two elements
+	a.back() = 999999;
+	a[a.size()-2] = 42;
+	
+	print(a, "modified: ");
+	
+	// process sum of all elements
+	std::cout << "sum: " << std::accumulate(a.begin(), a.end(), 0) << std::endl;
+	
+	std::transform(a.begin(), a.end(), a.begin(), std::negate<int>());
+	print(a, "negated: ");
 }
