df927c15e3aeff02d79dfd78a3f70cc7d3268d33
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-04-01 19:56:38 +0300

    set

--- Associative Containers/set1.cpp
+++ Associative Containers/set1.cpp
@@ -3,23 +3,26 @@
 
 int main()
 {
-	typedef std::set<int> IntSet;
+	std::set<int> c;
 	
-	IntSet coll;
+	c.insert(1);
+	c.insert(2);
+	c.insert(4);
+	c.insert(5);
+	c.insert(6);
 	
-	coll.insert(3);
-	coll.insert(1);
-	coll.insert(5);
-	coll.insert(4);
-	coll.insert(1);
-	coll.insert(6);
-	coll.insert(2);
-	coll.insert(2);
-	
-	IntSet::const_iterator pos;
-	for(pos = coll.cbegin(); pos != coll.cend(); ++pos)
-	{
-		std::cout << *pos << ' ';
-	}
+	std::cout << "lower_bound(3): " << *c.lower_bound(3) << std::endl;
+	std::cout << "upper_bound(3): " << *c.upper_bound(3) << std::endl;
+	std::cout << "equal_range(3): " << *c.equal_range(3).first << " " 
+									<< *c.equal_range(3).second << std::endl;
+			
 	std::cout << std::endl;
+	
+	std::cout << "lower_bound(5): " << *c.lower_bound(5) << std::endl;
+	std::cout << "upper_bound(5): " << *c.upper_bound(5) << std::endl;
+	std::cout << "equal_range(5): " << *c.equal_range(5).first << " "
+									<< *c.equal_range(5).second << std::endl;
+									
+
+	return 0;
 }
